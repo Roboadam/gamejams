f9b0d5c88a5d20a97a355fbbfcf9ecd4cf7dcf64
Adam H <adam@roboadam.com>
2019-08-25 23:16:22 -0400

    stash

---
+++ GhostPortal/Assets/Scripts/Ghost.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Ghost : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public GameObject floor;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        Mesh floorMesh = floor.GetComponent<Mesh>();
+        int index = Random.Range(0, floorMesh.vertices.Length - 1);
+        agent.destination = floorMesh.vertices[index];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
