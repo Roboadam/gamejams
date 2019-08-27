5406cf4019ec007c755f708706ee6cb518311d5d
Adam H <adam@roboadam.com>
2019-08-26 23:24:05 -0400

    AI

--- GhostPortal/Assets/Scripts/Ghost.cs
+++ GhostPortal/Assets/Scripts/Ghost.cs
@@ -6,19 +6,36 @@ using UnityEngine.AI;
 public class Ghost : MonoBehaviour
 {
     private NavMeshAgent agent;
-    public GameObject floor;
+    private bool reachedIntermediatePoint = false;
+    public GameObject player;
+
+    public int GhostIndex { get; set; } = 0;
+
+    private static Vector3[] intermediateStarts = new Vector3[] {
+        new Vector3(-0.96f, 1.54f, 13.04f),
+        new Vector3(-11.14f, 1.54f, 8.06f),
+        new Vector3(4.32f, 1.54f, 0.23f)
+    };
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-        Mesh floorMesh = floor.GetComponent<Mesh>();
-        int index = Random.Range(0, floorMesh.vertices.Length - 1);
-        agent.destination = floorMesh.vertices[index];
+        agent.destination = intermediateStarts[GhostIndex % intermediateStarts.Length];
+        Debug.Log("Destination " + agent.destination);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(!reachedIntermediatePoint && agent.remainingDistance < 0.01f)
+        {
+            reachedIntermediatePoint = true;
+            Debug.Log("Reached intermediate point");
+        }
+
+        if(reachedIntermediatePoint)
+        {
+            agent.destination = player.transform.position;
+        }
     }
 }
---
+++ GhostPortal/Assets/Scripts/Portal.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    public GameObject ghostPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time % 10 == 0)
+        {
+            GameObject ghostGameObject = Instantiate(ghostPrefab, new Vector3(6.82f, 0.84f, 4.82f), Quaternion.identity);
+            ghostGameObject.GetComponent<Ghost>().GhostIndex = 1;
+        }
+    }
+}
